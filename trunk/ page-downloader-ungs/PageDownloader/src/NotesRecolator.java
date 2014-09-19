377ab2075312d8237d1e7ac33151558e57b0fb37
j.godoy277@gmail.com <j.godoy277@gmail.com@d9c62697-70c4-97dd-d338-9bb8f2e38bd0>
2014-09-19 04:44:28 +0000

    agregado contador de tiempo de descarga / parsear

--- trunk/ page-downloader-ungs/PageDownloader/src/NotesRecolator.java
+++ trunk/ page-downloader-ungs/PageDownloader/src/NotesRecolator.java
@@ -20,29 +20,50 @@ public class NotesRecolator {
 			for (String archivo : carpeta.list()) {
 				File file = new File(carpeta.getAbsolutePath() + "//" + archivo);
 				if (file.isFile()) {
-					System.out.print("//PROCESANDO ARCHIVO N°: " + i);
+					System.out.println("//PROCESANDO ARCHIVO "+ archivo + ", N°: " + i);
 					long init = new Date().getTime();
 					// Obtener los links asociados a las notas de cada archivo
 					try {
 						Elements elem = Jsoup.parse(file, "utf-8").getElementById("archivo-notas-272")
 								.getElementsByTag("a").select("[href]");
+						long finParsearEnlacesANotas = new Date().getTime() - init;
+
+						int n = 1;
+						System.out.println("Este archivo tiene "+ elem.size() + "notas/artículos.");
 
 						for (Element E : elem) {
+							long inicioDescargarUnaNota = new Date().getTime();
 							Document doc = Jsoup.connect(E.attr("href")).timeout(0).get();
+							long finDescargarUnaNota = new Date().getTime() - inicioDescargarUnaNota;
+
+							long inicioParsearUnaNota = new Date().getTime();
+							//TODO: nullpointer! Aparentemente no encuentra "encabezado" (o h1??) VERIFICAR!!!
 							String titulo = doc.getElementById("encabezado").getAllElements().select("h1").text();
 							String descripcion = doc.getElementById("encabezado").getAllElements().select("p").text();
 							String cuerpo = doc.getElementById("cuerpo").getAllElements().select("p").text();
 							String articulo = titulo + "\n" + descripcion + "\n" + cuerpo;
+							long finParsearUnaNota = new Date().getTime() - inicioParsearUnaNota;
+
+							long inicioGuardarUnaNota = new Date().getTime();
 							String nombreArchivo = archivo.replace(".html", "_" + titulo);
 							StoreFile sf = new StoreFile(pathAGuardar, ".txt", articulo, nombreArchivo, "iso-8859-1");
 							sf.store();
+							long finGuardarUnaNota = new Date().getTime() - inicioGuardarUnaNota;
+
+							System.out.println("\t buscarEnlacesEnArchivo" + n + " : " + finParsearEnlacesANotas + "ms");
+							System.out.println("\t DescargarNota" + n + " : " + finDescargarUnaNota + " ms");
+							System.out.println("\t ParsearNota" + n + " : " + finParsearUnaNota + "ms");
+							System.out.println("\t GuardarNota" + n + " : " + finGuardarUnaNota + "ms");
+
+							n++;
 						}
 					} catch (IOException e) {
 						e.printStackTrace();
 						continue;
 					}
 					long now = new Date().getTime();
-					System.out.println(" - Tardó: " + (now - init) / 1000 + "seg.");
+					System.out.println(" - Tardó en total aprox: " + (now - init) / 1000 + "seg.");
+
 					i++;
 				}
 			}
